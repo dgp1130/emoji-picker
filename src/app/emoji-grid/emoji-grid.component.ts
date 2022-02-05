8c1f4b89b40af33e3abd0ed43cb1566a7b6f3f48
Doug Parker <dgp1130@users.noreply.github.com>
2022-02-04 20:21:08 -0800

    Copies emoji on click.
    The event listener is placed on the `mat-grid-list` element rather than the `mat-grid-tile` to use only one event listener instead of O(n) listeners. This gets a little awkward because does not retarget events, so the event target is actually a `<div />` under `mat-grid-tile`. I had to manually walk the DOM tree up until a found the right element to get the text from.

--- src/app/emoji-grid/emoji-grid.component.ts
+++ src/app/emoji-grid/emoji-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { EmojisService } from 'src/app/emojis.service';
 
 @Component({
@@ -7,7 +7,36 @@ import { EmojisService } from 'src/app/emojis.service';
   styleUrls: ['./emoji-grid.component.css']
 })
 export class EmojiGridComponent {
-  constructor(private emojisService: EmojisService) {}
+  constructor(private emojisService: EmojisService, private el: ElementRef) {}
 
   emojis = this.emojisService.listEmojis();
+
+  /** Copy emoji to clipboard on click. */
+  async onGridClicked(evt: Event): Promise<void> {
+    const target = evt.target as HTMLElement;
+    for (const el of retarget(target, this.el.nativeElement)) {
+      if (el.tagName.toLowerCase() !== 'mat-grid-tile') continue;
+
+      const glyph = el.textContent?.trim();
+      if (!glyph) throw new Error(`No glyph for grid tile, ${el}.`);
+
+      await navigator.clipboard.writeText(glyph);
+      return;
+    }
+  }
+}
+
+/**
+ * Angular doesn't support event retargetting, so a click event could come from
+ * a descendent implementation detail of a child component. Walk the DOM tree
+ * towards the root until we hit the given root (the `this` component) to emit
+ * all the nodes between an event target and the component.
+ */
+function* retarget(leaf: HTMLElement, root: HTMLElement):
+    Generator<HTMLElement, void, void> {
+  const parent = leaf.parentElement;
+  if (parent && parent !== root) {
+    yield leaf;
+    yield* retarget(parent, root);
+  }
 }
