21fdd2d8f70a4b7e40e8ae988ea715b81af65bac
Doug Parker <dgp1130@users.noreply.github.com>
2022-02-04 21:35:42 -0800

    Removes emoji index from `EmojisService`.
    This index is unused.

--- src/app/emojis.service.ts
+++ src/app/emojis.service.ts
@@ -5,42 +5,23 @@ import { Emoji } from './emoji';
   providedIn: 'root'
 })
 export class EmojisService {
-  private fetchedData?: Promise<{
-    emojis: Emoji[],
-    index: Map<string, Emoji[]>,
-  }>;
-  private fetchEmojis(): Promise<{
-    emojis: Emoji[],
-    index: Map<string, Emoji[]>,
-  }> {
+  private fetchedData?: Promise<Emoji[]>;
+  private fetchEmojis(): Promise<Emoji[]> {
     // Fetch and parse the emoji data if not already being fetched.
     return this.fetchedData ??= (async () => {
       const res = await fetch('/assets/emojis.json');
-      const json = await res.json();
-      const { emojis, index } = parseEmojis(json);
-      return { emojis, index };
+      const json = await res.json() as EmojiJson;
+      return json.emojis;
     })();
   }
 
   /** Return the list of all emojis. */
   public async listEmojis(): Promise<Emoji[]> {
-    const { emojis } = await this.fetchEmojis();
-    return emojis;
+    return await this.fetchEmojis();
   }
-}
-
-/** Parse the emoji JSON formatting. */
-function parseEmojis(emojiJson: EmojiJson): {
-  emojis: Emoji[],
-  index: Map<string, Emoji[]>,
-} {
-  const { emojis, index: indexRecord } = emojiJson;
-  const index = new Map<string, Emoji[]>(Object.entries(indexRecord));
-  return { emojis, index };
 }
 
 /** Type of the emoji data-transfer JSON object. */
 interface EmojiJson {
   emojis: Emoji[];
-  index: Record<string, Emoji[]>;
 }
